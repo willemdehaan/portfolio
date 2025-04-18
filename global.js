2a3ecf094e253259e2ba4bfa5fc7d626de45fd67
willemdehaan <wdehaan@ucsd.edu>
2025-04-18 11:43:44 -0700

    Update global.js
    Fixed Contacts link

--- global.js
+++ global.js
@@ -18,7 +18,7 @@ let pages = [
   { url: '', title: 'Home' },
   { url: 'projects/', title: 'Projects' },
   { url: 'resume/', title: 'Resume' },
-  { url: 'contact/', title: 'Contact' },
+  { url: 'contacts/', title: 'Contact' },
   { url: 'https://github.com/willemdehaan', title: 'GitHub' },
 ];
 
