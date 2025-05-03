8ce49ef97adb11dd3c9c330022a79eff3581dc74
willemdehaan <wdehaan@ucsd.edu>
2025-05-03 08:15:00 -0700

    Referencing Project Data

--- projects/projects.js
+++ projects/projects.js
@@ -16,16 +16,16 @@ document.addEventListener('DOMContentLoaded', async () => {
     titleElement.textContent = `${projects.length} Projects`;
   }
 
-// Sample data for pie chart
-// Define labeled data
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year
+);
+
+// Convert rolled data to desired format
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 
 // Create color scale
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
